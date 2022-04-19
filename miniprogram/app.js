fcbc8305bc96584fb13872bcf0857d7c87aab84d
Boliang <2021155841@qq.com>
2022-04-19 16:13:54 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,26 @@
+//app.js
+import { checkUpdateVersion } from "./utils";
+
+App({
+  onLaunch: function () {
+    
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      try {
+        wx.cloud.init({
+          traceUser: true,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // 检查版本
+    checkUpdateVersion();
+
+    this.globalData = {
+      version: "v1.7.9"
+    }
+  }
+})
